bdf1be715afd8d6602ba0c24f405755cc6c90930
chavi kothari <61689704+chavikothari2711@users.noreply.github.com>
2023-05-10 15:59:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 $ = function(id) {
   return document.getElementById(id);
 }
-// var signdets = ["signin:"];
-// var logindets = ["login:"];
 var show = function(id) {
 	$(id).style.display ='block';
 }
@@ -15,19 +13,3 @@ const myTimeout = setTimeout(Signup,10000);
 function Signup(){
     document.getElementById('signup').style.display="block";
 }
-//
-// function DetailsSignup(){
-//   var firstname = document.getElementById('fname').value;
-//   var lastname = document.getElementById('lname').value;
-//   var emailID = document.getElementById('signemail').value;
-//   var password = document.getElementById('signpass').value;
-//   signdets.push(firstname,lastname,emailID,password);
-//   console.log(signdets);
-// }
-//
-// function DetailsLogin(){
-//   var emailID = document.getElementById('loginemail').value;
-//   var password = document.getElementById('loginpass').value;
-//   logindets.push(emailID,password);
-//   console.log(logindets);
-// }
